21ffb43c77494ebfe9229eef4c0a5e87dea395c0
frankMarcelino <frank.silva.ti@gmail.com>
2023-09-18 16:05:15 -0300

    fim da aula 12

--- exercicios.js/ex012.js
+++
@@ -1,7 +0,0 @@
-var idade = 22
-
-if (idade < 18) {
-    console.log('MEnor de idade')
-}else{
-    console.log('Maior de idade')
-}
---
+++ exercicios.js/ex012a.js
@@ -0,0 +1,10 @@
+var idade = 12
+console.log(`você tem ${idade}.`)
+if (idade < 16) {
+    console.log('Não vota!')
+} else if (idade < 18 || idade > 65) { 
+        console.log('Voto opcionar!')
+} else {
+        console.log('Voto Obrigatorio!')
+} 
+ 
