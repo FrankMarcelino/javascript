dd7932b6395e49c6ab640bfa27733fc4099d9346
frankMarcelino <frank.silva.ti@gmail.com>
2023-09-21 17:41:04 -0300

    fim da aula 15 do modulo F

--- exercicios.js/ex014.js
+++ exercicios.js/ex014.js
@@ -1,5 +1,13 @@
+
 var c = 1
 do {
     console.log(`Passo ${c}`)
     c++ // a mesma coisa que c = c + 1
 } while (c <= 100)
+
+//--------------------------------------------
+/*console.log('Vai começar...')
+for(c = 1 ; c <= 10; c++){
+    console.log(`Passo ${c}`)
+}
+console.log('fim!')*/
