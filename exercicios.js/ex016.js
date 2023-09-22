9f49c972e7a9a3d06f349db2de770070ea887269
frankMarcelino <frank.silva.ti@gmail.com>
2023-09-22 10:50:58 -0300

    fim da aula 16

--- exercicios.js/ex016.js
+++ exercicios.js/ex016.js
@@ -7,15 +7,11 @@ function parimpar (n){
     } else {
         return 'impar'
     }
-
-
 }
 
 console.log(parimpar(1))
 
-
 // outro exemplo (soma)
-
 
 function soma( n1=0, n2=0 ){
     return n1 + n2
@@ -34,7 +30,7 @@ console.log(v(5))
 
 // outro exemplo (fatorial)
 
-function fatorial(n) {
+function fatorial1(n) {
     let fat = 1
     for(let contador = n; contador > 1; contador--){
         fat *= contador
@@ -42,4 +38,16 @@ function fatorial(n) {
     return fat
 }
 
-console.log(fatorial(4))
+console.log(fatorial1(5))
+
+// outro exemplo (fatorial) recursiva: quando uma função chama ela mesma
+
+function fatorial2(n) {
+    if (n == 1) {
+        return 1
+    } else {
+        return n * fatorial2(n-1)
+    }
+}
+
+console.log(fatorial2(5))
